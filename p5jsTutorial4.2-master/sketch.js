c5fc09ee718cb993dba1f71e971874232a445907
ps20221041 <42793229+ps20221041@users.noreply.github.com>
2018-11-05 12:34:03 +0800

    Add files via upload

---
+++ p5jsTutorial4.2-master/sketch.js
@@ -0,0 +1,52 @@
+var bgm;
+
+// declare sfx1 variable
+var sfx1;
+
+var buttonStart;
+
+function preload()
+{
+	bgm = loadSound("https://ps20221041.github.io/p5jsTutorial4.2/sounds/johnny-english.mp3");
+
+	// load sound file into variable sfx1
+	sfx1 = loadSound("https://ps20221041.github.io/p5jsTutorial4.2/sounds/zapsplat_multimedia_game_sound_positive_action_tone_032_25091.mp3");
+
+	buttonStart = loadImage("https://ps20221041.github.io/p5jsTutorial4.2/images/button_press-for-sfx.png");
+
+}
+
+function setup()
+{
+	createCanvas(800,600);
+
+	bgm.play();
+}
+
+function draw()
+{
+	textSize(12);
+
+	background(125,125,125)
+
+	text("Credit for bgm goes to Benjamin TISSOT from www.bensound.com",10,40);
+	text("Credit for sound effect goes to zapSplat from www.zapsplat.com",10,80);
+
+
+	image(buttonStart,200,100);
+
+	// Check if we have clicked on the button
+	if (mouseX > 200 && mouseX < 200 + 168 && mouseY > 100 && mouseY < 100 + 40 && mouseIsPressed)
+	{
+		// check that the sound effect is NOT (!) already playing; it is usually not desired to have the sound effect overlap
+		if (!sfx1.isPlaying())
+		{
+			// play the sound effect
+			sfx1.play();
+		}
+	}
+
+
+
+}
+
