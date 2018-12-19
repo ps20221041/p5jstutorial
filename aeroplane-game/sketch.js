17a41097a2b38d160a7aca45b4df6a8563d82c9b
ps20221041 <42793229+ps20221041@users.noreply.github.com>
2018-12-19 10:03:14 +0800

    Add files via upload

--- aeroplane-game/sketch.js
+++ aeroplane-game/sketch.js
@@ -15,6 +15,21 @@ var mediumquestion3;
 var mediumquestion4;
 var mediumquestion5;
 var mediumquestion6;
+var mediumquestion7;
+var mediumquestion8;
+var mediumquestion9;
+var mediumquestion10;
+
+var hardquestion1;
+var hardquestion2;
+var hardquestion3;
+var harduestion4;
+var hardquestion5;
+var hardquestion6;
+var hardquestion7;
+var hardquestion8;
+var hardquestion9;
+var hardquestion10;
 
 var questionX;
 var questionY;
@@ -22,8 +37,14 @@ var questionLength;
 var questionHeight;
 var introImage;
 
+var jetFlyBy;
+var correctSound;
+var incorrectSound;
+
 var canvasID;
 var locked;
+
+var score;
 
 var numberOfAnswers;
 
@@ -36,6 +57,10 @@ function preload()
 
 	questionX = [27,27+250+50,27,27+250+50];
 	questionY = [50,50,50+200+23,50+200+23];
+
+	jetFlyBy = loadSound("https://ps20221041.github.io/p5jstutorial/aeroplane-game/sounds/jetflyby.mp3")
+	correctSound = loadSound("https://ps20221041.github.io/p5jstutorial/aeroplane-game/sounds/correct.mp3")
+	incorrectSound = loadSound("https://ps20221041.github.io/p5jstutorial/aeroplane-game/sounds/incorrect.mp3")
 
 
 	introImage = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_other/intro.png');
@@ -51,16 +76,31 @@ function preload()
 	easyquestion9 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_easy/109.png');
 	easyquestion10 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_easy/110.png');
 
-	mediumquestion1 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_easy/201.png');
-	mediumquestion2 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_easy/202.png');
-	mediumquestion3 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_easy/203.png');
-	mediumquestion4 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_easy/204.png');
-	mediumquestion5 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_easy/205.png');
-	mediumquestion6 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_easy/206.png');
+	mediumquestion1 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/201.png');
+	mediumquestion2 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/202.png');
+	mediumquestion3 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/203.png');
+	mediumquestion4 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/204.png');
+	mediumquestion5 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/205.png');
+	mediumquestion6 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/206.png');
+	mediumquestion7 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/207.png');
+	mediumquestion8 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/208.png');
+	mediumquestion9 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/209.png');
+	mediumquestion10 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_medium/210.png');
 
+	hardquestion1 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/301.png');
+	hardquestion2 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/302.png');
+	hardquestion3 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/303.png');
+	hardquestion4 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/304.png');
+	hardquestion5 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/305.png');
+	hardquestion6 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/306.png');
+	hardquestion7 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/307.png');
+	hardquestion8 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/308.png');
+	hardquestion9 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/309.png');
+	hardquestion10 = loadImage('https://ps20221041.github.io/p5jstutorial/aeroplane-game/backgrounds_hard/310.png');
 
 
 }
+
 
 function setup()
 {
@@ -69,7 +109,37 @@ function setup()
 	score = 0;
 	canvasID = 100;
 	locked = false;
+	if (!jetFlyBy.isPlaying())
+	{
+		jetFlyBy.play();
+	}
 }
+
+
+function drawIntro()
+{
+	image(introImage,0,0);
+	if (mouseX > 300 & mouseX < 500 && mouseY > 200 & mouseY < 300 && mouseIsPressed)
+	{
+		canvasID = 101;
+		score = 0;
+
+	}
+	if (mouseX > 300 & mouseX < 500 && mouseY > 330 & mouseY < 430 && mouseIsPressed)
+	{
+		canvasID = 201;
+		score = 0;
+
+	}
+	if (mouseX > 300 & mouseX < 500 && mouseY > 460 & mouseY < 560 && mouseIsPressed)
+	{
+		canvasID = 301;
+		score = 0;
+
+	}
+
+}
+
 
 function draw()
 {
@@ -86,107 +156,196 @@ function draw()
 	else if (canvasID == 101)
 	{
 		image(easyquestion1,0,0);
-
-//function isButtonClicked(correct,nextID)
-
 		isButtonClicked(1,102)
+		showScore();
+
 	}
 	else if (canvasID == 102)
 	{
 		image(easyquestion2,0,0);
 		isButtonClicked(1,103)
+		showScore();
+
 	}
 	else if (canvasID == 103)
 	{
 		image(easyquestion3,0,0);
 		isButtonClicked(3,104)
+		showScore();
 	}
 	else if (canvasID == 104)
 	{
 		image(easyquestion4,0,0);
 		isButtonClicked(2,105)
+		showScore();
 	}
 	else if (canvasID == 105)
 	{
 		image(easyquestion5,0,0);
 		isButtonClicked(3,106)
+		showScore();
 	}
 	else if (canvasID == 106)
 	{
 		image(easyquestion6,0,0);
 		isButtonClicked(2,107)
+		showScore();
 	}
 	else if (canvasID == 107)
 	{
 		image(easyquestion7,0,0);
 		isButtonClicked(4,108)
+		showScore();
 	}
 	else if (canvasID == 108)
 	{
 		image(easyquestion8,0,0);
 		isButtonClicked(1,109)
+		showScore();
 	}
 	else if (canvasID == 109)
 	{
 		image(easyquestion9,0,0);
 		isButtonClicked(2,110)
+		showScore();
 	}
 	else if (canvasID == 110)
 	{
 		image(easyquestion10,0,0);
 		isButtonClicked(3,100)
+		showScore();
 	}
+
 
 	else if (canvasID == 201)
 	{
 		image(mediumquestion1,0,0);
 		isButtonClicked(3,202)
+		showScore();
 	}
 	else if (canvasID == 202)
 	{
 		image(mediumquestion2,0,0);
 		isButtonClicked(3,203)
+		showScore();
 	}
 	else if (canvasID == 203)
 	{
 		image(mediumquestion3,0,0);
 		isButtonClicked(1,204)
+		showScore();
 	}
 	else if (canvasID == 204)
 	{
 		image(mediumquestion4,0,0);
 		isButtonClicked(4,205)
+		showScore();
 	}
 	else if (canvasID == 205)
 	{
 		image(mediumquestion5,0,0);
 		isButtonClicked(2,206)
+		showScore();
 	}
 	else if (canvasID == 206)
 	{
-		image(mediumquestion6,3,0);
-		isButtonClicked(2,207)
+		image(mediumquestion6,0,0);
+		isButtonClicked(3,207)
+		showScore();
+	}
+	else if (canvasID == 207)
+	{
+		image(mediumquestion7,0,0);
+		isButtonClicked(4,208)
+		showScore();
+	}
+	else if (canvasID == 208)
+	{
+		image(mediumquestion8,0,0);
+		isButtonClicked(3,209)
+		showScore();
+	}
+	else if (canvasID == 209)
+	{
+		image(mediumquestion9,0,0);
+		isButtonClicked(1,210)
+		showScore();
+	}
+	else if (canvasID == 210)
+	{
+		image(mediumquestion10,0,0);
+		isButtonClicked(4,100)
+		showScore();
 	}
 
+
+	else if (canvasID == 301)
+	{
+		image(hardquestion1,0,0);
+		isButtonClicked(3,302)
+		showScore();
+	}
+	else if (canvasID == 302)
+	{
+		image(hardquestion2,0,0);
+		isButtonClicked(4,303)
+		showScore();
+	}
+	else if (canvasID == 303)
+	{
+		image(hardquestion3,0,0);
+		isButtonClicked(1,304)
+		showScore();
+	}
+	else if (canvasID == 304)
+	{
+		image(hardquestion4,0,0);
+		isButtonClicked(3,305)
+		showScore();
+	}
+	else if (canvasID == 305)
+	{
+		image(hardquestion5,0,0);
+		isButtonClicked(2,306)
+		showScore();
+	}
+	else if (canvasID == 306)
+	{
+		image(hardquestion6,0,0);
+		isButtonClicked(4,307)
+		showScore();
+	}
+	else if (canvasID == 307)
+	{
+		image(hardquestion7,0,0);
+		isButtonClicked(2,308)
+		showScore();
+	}
+	else if (canvasID == 308)
+	{
+		image(hardquestion8,0,0);
+		isButtonClicked(3,309)
+		showScore();
+	}
+	else if (canvasID == 309)
+	{
+		image(hardquestion9,0,0);
+		isButtonClicked(1,310)
+		showScore();
+	}
+	else if (canvasID == 310)
+	{
+		image(hardquestion10,0,0);
+		isButtonClicked(3,100)
+		showScore();
+	}
 
 }
 
-function drawIntro()
+function showScore()
 {
-	image(introImage,0,0);
-	if (mouseX > 300 & mouseX < 500 && mouseY > 200 & mouseY < 300 && mouseIsPressed)
-	{
-		canvasID = 101;
-	}
-	if (mouseX > 300 & mouseX < 500 && mouseY > 330 & mouseY < 430 && mouseIsPressed)
-	{
-		canvasID = 201;
-	}
-	if (mouseX > 300 & mouseX < 500 && mouseY > 460 & mouseY < 560 && mouseIsPressed)
-	{
-		canvasID = 301;
-	}
-
+	fill(0,0,0);
+	text(score,10,10);
 }
 
 
@@ -204,18 +363,31 @@ function isButtonClicked(correct,nextID)
 			{
 				if (i == correct)
 				{
+					if (!correctSound.isPlaying())
+					{
+						correctSound.play();
+					}
+					score += 10;
 					canvasID = nextID;
+	
 					return 0;
 				}
 				else
 				{
 					deductHealth = true;
+
 				}
 			}
 		}
 		if (deductHealth)
 		{
-		//	health = health - 50;
+			score -= 0;
+			if (!incorrectSound.isPlaying())
+			{
+				incorrectSound.play();
+			}
+			canvasID = nextID;
+
 		}
 	}
 }
